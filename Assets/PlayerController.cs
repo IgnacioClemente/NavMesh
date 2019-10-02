b3c78610696539c1653ba4bb7532e97c4ce728d2
hansow <nachoagus@live.com.ar>
2019-10-02 16:09:46 -0300

    no message

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] Transform pos;
+    [SerializeField] NavMeshAgent agent;
+    [SerializeField] Transform[] DestinationPoints;
+
+    void Start()
+    {
+        agent.SetDestination(pos.position);
+    }
+
+    void Update()
+    {
+        Patrol();
+    }
+
+    public void Patrol()
+    {
+        for (int i = 0; i < DestinationPoints.Length; i++)
+        {
+            agent.SetDestination(DestinationPoints[i].position);
+        }
+    }
+}
