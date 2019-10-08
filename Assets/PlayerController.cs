577fd28479a8d2242e6cbca8addaecfea160f861
hansow <nachoagus@live.com.ar>
2019-10-08 16:51:32 -0300

    no message

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -3,27 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class PlayerController : MonoBehaviour
-{
-    [SerializeField] Transform pos;
-    [SerializeField] NavMeshAgent agent;
+public class PlayerController : MonoBehaviour { 
+    
     [SerializeField] Transform[] DestinationPoints;
+
+    Transform actualDestination;
+    NavMeshAgent agent;
+
+    //elegir un destino aleatorio y sitearlo como el destino actual, calcular la distancia al destino actual y cuando llego elegir un nuevo destino aleatoria
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
 
     void Start()
     {
-        agent.SetDestination(pos.position);
+        ChooseNewDestination();
     }
 
     void Update()
     {
-        Patrol();
+        var distance = Vector3.Distance(transform.position, actualDestination.position);
+        print(distance);
+        if (distance < 0.8f)
+            ChooseNewDestination();
     }
 
-    public void Patrol()
+    public void ChooseNewDestination()
     {
-        for (int i = 0; i < DestinationPoints.Length; i++)
-        {
-            agent.SetDestination(DestinationPoints[i].position);
-        }
+        int rdm = Random.Range(0, DestinationPoints.Length);
+        actualDestination = DestinationPoints[rdm];
+        agent.SetDestination(actualDestination.position);
     }
 }
